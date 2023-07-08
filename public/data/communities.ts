f79d2961991b2aa3ea6f9306d1496f168429c931
shakiroye <oyeossiadewoye@gmail.com>
2023-07-08 14:02:16 +0000

    feat: add communities data

--- public/data/communities.ts
+++ public/data/communities.ts
@@ -1,13 +1,144 @@
-import { CommunitiesInterface } from "interfaces";
+import { CommunityInterface } from "interfaces";
 
-export const communities: CommunitiesInterface[] = [
+export const communities: CommunityInterface[] = [
   {
     name: "TDev",
-    description:
-      "Une communauté qui tend à promouvoir les TICs et favoriser l'entraide entre les développeurs.",
-    socials: {
+    links: {
+      telegram: "https://t.me/tdev228",
+      twitter: "https://twitter.com/tdev228",
+      discord: "https://discord.gg/KasJJJSu",
       website: "https://www.tdev228.org",
-      telegram: "tdev228",
+      github: "https://github.com/Tdev228",
+      linkedin: "https://www.linkedin.com/company/tdev228",
+      facebook: "https://www.facebook.com/Tdev228",
+    },
+  },
+  {
+    name: "TogoDevelopers",
+    links: {
+      telegram: "https://t.me/togodevelopers",
+      twitter: "https://twitter.com/togo_developers",
+      website: "https://togodevelopers.com",
+      github: "https://github.com/Togodevelopers",
+      linkedin: "https://www.linkedin.com/company/togo-developers",
+      facebook: "https://www.facebook.com/togodevelopers",
+    },
+  },
+  {
+    name: "FlutterTg",
+    links: {
+      telegram: "https://t.me/flutterTg",
+      github: "https://github.com/FlutterTogo",
+      twitter: "https://twitter.com/FlutterTg",
+      whatsapp: "https://chat.whatsapp.com/GpJLokAUIZR1rJZb8sQ2HW",
+    },
+  },
+  {
+    name: "Togo Designers",
+    links: {
+      website: "https://design.tg",
+      twitter: "https://twitter.com/TgDesigners",
+    },
+  },
+  {
+    name: "Friend Of Figma Lomé",
+    links: {
+      telegram: "https://t.me/friendsoffigmalome",
+      twitter: "https://twitter.com/fof_lome",
+      website: "https://friends.figma.com/lome-togo",
+    },
+  },
+  {
+    name: "WordPress TG",
+    links: {
+      telegram: "https://t.me/WordPressTG",
+      meetup: "https://www.meetup.com/fr-FR/WordPressLome",
+    },
+  },
+  {
+    name: "NIGHT CODING",
+    links: {
+      whatsapp: "https://chat.whatsapp.com/FDonlhXxYT8FZbo1SRMvZ6",
+      telegram: "https://t.me/night_coding_meet",
+    },
+  },
+  {
+    name: "GDSC Université de Lomé",
+    links: {
+      whatsapp: "https://chat.whatsapp.com/ERxX6zzHJRHFOhIghqwFuF",
+      telegram: "https://t.me/gdsc_ul",
+    },
+  },
+  {
+    name: "Zindi Togo",
+    links: {
+      whatsapp: "https://chat.whatsapp.com/CIc7VuG5WtEBjpVt29fkTa",
+    },
+  },
+  {
+    name: "OSCA Lomé",
+    links: {
+      whatsapp: "https://chat.whatsapp.com/GIu98zL9iHU9nwUC9EXBTx",
+    },
+  },
+  {
+    name: "Microsoft Learn Student Ambassadors Togo (MLSA Togo)",
+    links: {
+      twitter: "https://twitter.com/mlsatogo",
+      youtube: "https://youtube.com/@mlsatogo",
+      whatsapp: "https://chat.whatsapp.com/Ji0d7JYIDF65PL6UaqLKFv",
+    },
+  },
+  {
+    name: "TgTech 🇹🇬",
+    links: {
+      telegram: "https://t.me/TgTech",
+    },
+  },
+  {
+    name: "Hitech 🖥📱⌚️",
+    links: {
+      telegram: "https://t.me/HiTech_Group",
+      whatsapp: "https://chat.whatsapp.com/FAuuVFOmWmv5zuBQmJhUT4",
+      twitter: "https://twitter.com/WordpressTG",
+    },
+  },
+  {
+    name: "TgLINUX🐧| gnu/linux🇫🇷-Tchat👥",
+    links: {
+      telegram: "https://t.me/TGLinux",
+    },
+  },
+  {
+    name: "Open Data Togo",
+    links: {
+      twitter: "https://twitter.com/opendatatg",
+    },
+  },
+  {
+    name: "OpenStreetMap Togo (OSM Togo)",
+    links: {
+      telegram: "https://t.me/OsmtogoGroup",
+      website: "https://openstreetmap.tg/",
+      twitter: "https://twitter.com/osmtogo",
+      facebook: "https://www.facebook.com/OpenstreetmapTogo",
+    },
+  },
+  {
+    name: "PyTogo Community",
+    links: {
+      telegram: "https://t.me/pythontogo",
+      website: "https://www.pythontogo.org/",
+      twitter: "https://twitter.com/PythonTg",
+      facebook: "https://www.facebook.com/PythonTg/",
+    },
+  },
+  {
+    name: "OpenData Togo",
+    links: {
+      telegram: "https://t.me/opendatatg",
+      twitter: "https://twitter.com/opendatatg",
+      github: "https://github.com/odtt",
     },
   },
 ];
