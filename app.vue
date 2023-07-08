30be3edc9cfd211778c7c6df964278db7c5a870d
shakiroye <oyeossiadewoye@gmail.com>
2023-07-08 23:13:45 +0000

    chore: add meta datas

--- app.vue
+++ app.vue
@@ -1,3 +1,31 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  charset: "utf-8",
+  title: "Togo Tech Communities",
+  meta: [
+    {
+      name: "description",
+      content: "Discover the Thriving Tech Communities shaping Togo's Future.",
+    },
+    {
+      property: "og:image",
+      content: "https://togo-tech-communities.netlify.app/social-card.png",
+    },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:creator", content: "@shakiroye" },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/svg",
+      href: "/community.svg",
+    },
+  ],
+});
+</script>
 <template>
   <div>
     <Navbar />
