72390322874dae103ffcd4794b94ae4a67b26ef6
shakiroye <oyeossiadewoye@gmail.com>
2023-07-08 14:02:30 +0000

    chore: update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,13 +2,17 @@
 <template>
   <nav class="w-full bg-white dark:bg-primary-900 flex justify-between">
     <NuxtLink to="/" class="flex items-center gap-3">
-      <h1 class="font-bold text-xl">Togo Tech Communities</h1>
+      <i
+        class="i-fluent-people-community-20-regular h-8 w-8 transition duration-500 ease-in-out text-primary-700"
+      ></i>
+      <h1 class="font-bold text-xl text-primary-700">Togo Tech Communities</h1>
     </NuxtLink>
     <NuxtLink
       to="/dashboard"
-      class="bg-primary-700 py-2 px-3 text-white rounded-xl"
+      class="bg-primary-700 py-2 px-3 text-white rounded-lg flex items-center gap-2"
     >
-      Add a community
+      <i class="i-bxl-github h-6 w-6 transition duration-500 ease-in-out"></i>
+      <small> Contribute to the project </small>
     </NuxtLink>
   </nav>
 </template>
