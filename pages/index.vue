00040959796b4b0c2fd85a5bca822832c3d20b2a
shakiroye <oyeossiadewoye@gmail.com>
2023-07-23 22:40:48 +0000

    chore: refactor

--- pages/index.vue
+++ pages/index.vue
@@ -25,47 +25,45 @@ const filterCommunities = (searchQuery: string) => {
 </script>
 
 <template>
-  <div>
-    <main>
-      <div class="text-center mb-5 flex flex-col items-center gap-5">
-        <h1 class="text-3xl sm:text-5xl md:text-7xl font-extrabold">
-          Togo Tech <br />
-          Communities
-        </h1>
-        <p class="text-gray-500">
-          Discover the Thriving Tech Communities shaping Togo's Future.
-        </p>
+  <main>
+    <div class="text-center mb-5 flex flex-col items-center gap-5">
+      <h1 class="text-3xl sm:text-5xl md:text-7xl font-extrabold">
+        Togo Tech <br />
+        Communities
+      </h1>
+      <p class="text-gray-500">
+        Discover the Thriving Tech Communities shaping Togo's Future.
+      </p>
 
-        <SearchInput @search="filterCommunities" />
-      </div>
-      <div
-        class="w-full z-50 rounded-lg px-3 py-10 grid grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 gap-4"
-        v-if="!isSearchResultsEmpty"
+      <SearchInput @search="filterCommunities" />
+    </div>
+    <div
+      class="w-full z-50 rounded-lg px-3 py-10 grid grid-flow-row-dense sm:grid-cols-2 lg:grid-cols-3 gap-4"
+      v-if="!isSearchResultsEmpty"
+    >
+      <CommunityCard
+        :community="community"
+        v-for="(community, index) in filteredCommunities"
+        :key="index"
+      />
+    </div>
+
+    <div
+      v-else
+      class="flex items-center justify-center self-center my-14 mx-auto"
+    >
+      <p
+        class="text-center text-xl sm:text-2xl md:tsext-3xl font-medium opacity-80"
       >
-        <CommunityCard
-          :community="community"
-          v-for="(community, index) in filteredCommunities"
-          :key="index"
-        />
-      </div>
+        Sorry, we did not find any results for " {{ searchTerm }} "
+      </p>
+    </div>
 
-      <div
-        v-else
-        class="flex items-center justify-center self-center my-14 mx-auto"
-      >
-        <p
-          class="text-center text-xl sm:text-2xl md:tsext-3xl font-medium opacity-80"
-        >
-          Sorry, we did not find any results for " {{ searchTerm }} "
-        </p>
-      </div>
-
-      <div
-        class="fixed -z-50 right-16 -bottom-44 w-1/4 h-1/2 bg-gradient-to-r from-primary-400/20 to-cyan-400/20 blur-3xl rounded-full"
-      ></div>
-      <div
-        class="dark:fixed -z-50 left-0 bottom-0 w-full h-full bg-gradient-to-r from-primary-400/10 to-cyan-400/10 blur-sm"
-      ></div>
-    </main>
-  </div>
+    <div
+      class="fixed -z-50 right-16 -bottom-44 w-1/4 h-1/2 bg-gradient-to-r from-primary-400/20 to-cyan-400/20 blur-3xl rounded-full"
+    ></div>
+    <div
+      class="dark:fixed -z-50 left-0 bottom-0 w-full h-full bg-gradient-to-r from-primary-400/10 to-cyan-400/10 blur-sm"
+    ></div>
+  </main>
 </template>
